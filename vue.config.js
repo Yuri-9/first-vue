236742a8564e2595b1cfaf7851008f22faee37a4
Yury Vashkevich <yury_vashkevich1@epam.com>
2023-02-22 17:38:11 +0100

    feat/add vue.config and prettier

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,26 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './',
+  outputDir: 'dist',
+  productionSourceMap: false,
+  assetsDir: 'assets',
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+    svgRule.use('vye-svg-loader').loader('vye-svg-loader');
+  },
+  css: {
+    extract: true,
+    sourceMap: false,
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "@/variables.scss";',
+        sassOptions: {
+          indentWidth: 4,
+        },
+      },
+    },
+  },
 });
