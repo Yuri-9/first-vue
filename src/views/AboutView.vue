a8780a9de9acce1db5489c0c674fa6120fae405f
Yury Vashkevich <yury_vashkevich1@epam.com>
2023-02-23 10:39:53 +0100

    feat/add storybook

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <MyButton>My button About</MyButton>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MyButton from '@/components/MyButton.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    MyButton,
+  },
+});
+</script>
