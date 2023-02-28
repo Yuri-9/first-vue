7812a81257dae5315b683bc9e041fc855fa57df0
Yury Vashkevich <yury_vashkevich1@epam.com>
2023-02-28 22:21:31 +0100

    fix/change logo and varibles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="../assets/logo.svg" />
     <Button name="search">Search</Button>
     <Switcher title="Search by" :options="filters" @onSelect="setSearchBy" />
     <Search @onSearch="handleSearch" />
